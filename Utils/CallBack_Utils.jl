ec9447a0d060b045549ca3fb1635f0d526a09d81
Artyom91150 <artyom91150@gmail.com>
2023-04-10 14:40:42 +0300

    Update CallBack_Utils.jl

--- Utils/CallBack_Utils.jl
+++ Utils/CallBack_Utils.jl
@@ -28,32 +28,5 @@ function Get_callback(Condition, ODELenght)
     observer_array = MySavedValues(Float64, NTuple{ODELenght, Float64})
     affectThis! = MySavingAffect(observer_array)
 
-    Condition = parseCond(Condition)
     return [ContinuousCallback(Condition, affectThis!, affectThis!), observer_array]
-end
-
-# Parse anonymous function from config. file into function with signature func(x, t, integrator)
-function parseCond(f::Function)
-    nargs = methods(f)[1].nargs - 1
-    if  nargs == 3
-        return f
-
-    elseif nargs == 2
-        return ParsedCondition2(x, t, integrator) = Base.invokelatest(f, x, t)
-
-    elseif nargs == 1
-        return ParsedCondition1(x, t, integrator) = Base.invokelatest(f, x)
-
-    else
-        @warn "Invalid callback condition"
-        return nothing
-    end
-end
-
-function parseCond(f::Expr)
-    return parseCond(eval(f))
-end
-
-function parseCond(f::String)
-    return parseCond(Meta.parse(f))
 end
