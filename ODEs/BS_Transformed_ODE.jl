ce352f6488cc03e332b5137fdfd62fd6bd7db3d3
artyom91150 <artyom91150@gmail.com>
2023-09-13 22:52:02 +0300

    Update

--- ODEs/BS_Transformed_ODE.jl
+++ ODEs/BS_Transformed_ODE.jl
@@ -27,16 +27,16 @@ Z2Phi(z::String) = L"2atan(exp(\frac{%$z}{2}))"
     dx1 = (A * (th2 - th3) - B * th1 + C) - Eps * (sech(y1) * sh1 - th4)
     dx2 = (A * (th3 - th1) - B * th2 + C) - Eps * (sech(y2) * sh2 - th5) 
     dx3 = (A * (th1 - th2) - B * th3 + C) - Eps * (sech(y3) * sh3 - th6)
-    dy1 = (-A * (th5 - th6) - B * th4 + C) - Eps * (sech(x1) * sh4 - th1)
-    dy2 = (-A * (th6 - th4) - B * th5 + C) - Eps * (sech(x2) * sh5 - th2)
-    dy3 = (-A * (th4 - th5) - B * th6 + C) - Eps * (sech(x3) * sh6 - th3)
+    dx4 = (-A * (th5 - th6) - B * th4 + C) - Eps * (sech(x1) * sh4 - th1)
+    dx5 = (-A * (th6 - th4) - B * th5 + C) - Eps * (sech(x2) * sh5 - th2)
+    dx6 = (-A * (th4 - th5) - B * th6 + C) - Eps * (sech(x3) * sh6 - th3)
 
-    return T(dx1, dx2, dx3, dy1, dy2, dy3)
+    return T([dx1, dx2, dx3, dx4, dx5, dx6])
 end
 
 ### The same system but with x/2
 @inbounds function BS_Log_2(u::T, p, t=0.0) where T
-    u /= 2.0
+    u = u ./ 2.0
     x1, x2, x3, y1, y2, y3 = u
     A, B, C, Eps = p
 
@@ -46,11 +46,11 @@ end
     dx1 = 2.0 * (A * (th2 - th3) - B * th1 + C) - Eps * (sech(y1) * sh1 - th4)
     dx2 = 2.0 * (A * (th3 - th1) - B * th2 + C) - Eps * (sech(y2) * sh2 - th5) 
     dx3 = 2.0 * (A * (th1 - th2) - B * th3 + C) - Eps * (sech(y3) * sh3 - th6)
-    dy1 = 2.0 * (-A * (th5 - th6) - B * th4 + C) - Eps * (sech(x1) * sh4 - th1)
-    dy2 = 2.0 * (-A * (th6 - th4) - B * th5 + C) - Eps * (sech(x2) * sh5 - th2)
-    dy3 = 2.0 * (-A * (th4 - th5) - B * th6 + C) - Eps * (sech(x3) * sh6 - th3)
+    dx4 = 2.0 * (-A * (th5 - th6) - B * th4 + C) - Eps * (sech(x1) * sh4 - th1)
+    dx5 = 2.0 * (-A * (th6 - th4) - B * th5 + C) - Eps * (sech(x2) * sh5 - th2)
+    dx6 = 2.0 * (-A * (th4 - th5) - B * th6 + C) - Eps * (sech(x3) * sh6 - th3)
 
-    return T(dx1, dx2, dx3, dy1, dy2, dy3)
+    return T([dx1, dx2, dx3, dx4, dx5, dx6])
 end
 
 
@@ -62,11 +62,11 @@ end
     dx1 = sin(phi1) * (A * (cos(phi3) - cos(phi2)) + B * cos(phi1) + C - Eps * cos(β + psi1))
     dx2 = sin(phi2) * (A * (cos(phi1) - cos(phi3)) + B * cos(phi2) + C - Eps * cos(β + psi2))
     dx3 = sin(phi3) * (A * (cos(phi2) - cos(phi1)) + B * cos(phi3) + C - Eps * cos(β + psi3))
+    dx4 = sin(psi1) * (-A * (cos(psi3) - cos(psi2)) + B * cos(psi1) + C - Eps * cos(β + phi1))
+    dx5 = sin(psi2) * (-A * (cos(psi1) - cos(psi3)) + B * cos(psi2) + C - Eps * cos(β + phi2))
+    dx6 = sin(psi3) * (-A * (cos(psi2) - cos(psi1)) + B * cos(psi3) + C - Eps * cos(β + phi3))
 
-    dy1 = sin(psi1) * (-A * (cos(psi3) - cos(psi2)) + B * cos(psi1) + C - Eps * cos(β + phi1))
-    dy2 = sin(psi2) * (-A * (cos(psi1) - cos(psi3)) + B * cos(psi2) + C - Eps * cos(β + phi2))
-    dy3 = sin(psi3) * (-A * (cos(psi2) - cos(psi1)) + B * cos(psi3) + C - Eps * cos(β + phi3))
-    return T(dx1, dx2, dx3, dy1, dy2, dy3)
+    return T([dx1, dx2, dx3, dx4, dx5, dx6])
 end
 
 end
